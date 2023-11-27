febd64268f44272bc9020b9a9eb1ad493dcf0a15
Chaonan Li <licn@mtc.edu.cn>
2023-11-27 15:20:17 +0800

    fix bugs in plot_bmap()

--- R/map-visualize.R
+++ R/map-visualize.R
@@ -88,9 +88,9 @@ plot_bmap = function(map, var = NULL, ord = NULL, fill = NA, color = 'gray30', w
     }
 
     # Automatically generate color values if `fill == 'auto' and `var != NULL`
-    if (any(!is.na(fill)) && fill == 'auto' && !is.null(var) && is.numeric(map@data[,var])){
+    if (any(!is.na(fill)) && any(fill == 'auto') && !is.null(var) && is.numeric(map@data[,var])){
         fill <- colorRampPalette(RColorBrewer::brewer.pal(11, "RdYlGn"))(cla.len) %>% rev()
-    }else if (any(!is.na(fill)) && fill == 'auto' && !is.null(var) &&
+    }else if (any(!is.na(fill)) && any(fill == 'auto') && !is.null(var) &&
         (is.character(map@data[,var]) || is.factor(map@data[,var]))){
         fill <- randomcoloR::randomColor(count = cla.len)
     }
